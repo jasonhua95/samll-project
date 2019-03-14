92bf86e1972e82abe9df035dbe48e220b7eb4b85
华营杰 <jasonhua12@163.com>
2019-03-14 17:42:13 +0800

    分布式缓存

---
+++ DistributedSession/DistributedSession/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace DistributedRedis.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region 内存中缓存
+        //private IMemoryCache _cache;
+
+        //public HomeController(IMemoryCache memoryCache)
+        //{
+        //    _cache = memoryCache;
+        //}
+        #endregion
+
+        private readonly IDistributedCache _cache;
+        public HomeController(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult SetSession(string key, string value)
+        {
+            //_cache.Set(key, value); //内存中缓存
+
+            _cache.SetString(key, value);
+            return Json(new { success = true });
+        }
+
+        [HttpGet]
+        public IActionResult GetSession(string key)
+        {
+            //string value = _cache.Get<string>(key); //内存中缓存
+
+            string value = _cache.GetString(key);
+            return Json(new { success = true, data = value });
+        }
+    }
+}
---
+++ DistributedSession/DistributedSession/Startup.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DistributedRedis
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ////内存中缓存,完全可以替代session
+            //services.AddMemoryCache();
+
+            //redis分布式缓存
+            services.AddDistributedRedisCache(options =>
+            {
+                options.Configuration = "127.0.0.1:32768";//redis的端口地址
+                options.InstanceName = "simple";
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
