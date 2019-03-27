47df4375652a7c6d6f85472cd8386d2444638c3f
华营杰 <jasonhua12@163.com>
2019-03-27 12:03:38 +0800

    RabbitMQ 学习

---
+++ RabbitMQDemo/ReceiveMQ/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+/// <summary>
+/// 发送信息
+/// </summary>
+namespace ReceiveMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("销售者开始运行");
+
+            var factory = new ConnectionFactory() { HostName = "localhost", Port = 32850, UserName = "test", Password = "test123" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                //模拟接受者
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("接收信息：{0}", message);
+                };
+
+                while (true) {
+                    channel.BasicConsume(queue: "hello",
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                    Thread.Sleep(TimeSpan.FromSeconds(10));
+                    Console.WriteLine("每隔10秒接收一次");
+                }
+            }
+
+        }
+    }
+}
---
+++ RabbitMQDemo/SendMQ/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+/// <summary>
+/// 接收信息
+/// </summary>
+namespace SendMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("生产者开始运行");
+
+            var factory = new ConnectionFactory() { HostName = "localhost", Port = 32850, UserName = "test", Password = "test123" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                //模拟生成者
+                Random rd = new Random();
+                while (true)
+                {
+                    string message = $"你好，{rd.Next(10)}。";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                            routingKey: "hello",
+                                            basicProperties: null,
+                                            body: body);
+
+                    Console.WriteLine("发送信息：{0}", message);
+
+                    //随机休眠
+                    Thread.Sleep(TimeSpan.FromSeconds(rd.Next(5)));
+                }
+            }
+
+        }
+    }
+}
