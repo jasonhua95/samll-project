08e4002d39dd43fbe17057cbcf8c38944e0d27a4
华营杰 <jasonhua12@163.com>
2019-04-02 09:54:53 +0800

    工具类

---
+++ Utils/Utils/Main.cs
@@ -0,0 +1,24 @@
+﻿namespace Utils
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+    using log4net;
+    using Vanilla;
+
+    internal class Main
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        internal static async Task Work(string[] args)
+        {
+            await Task.FromResult(0);
+            //Logger.Info("Hello huayingjie! CLR:4.0.30319.42000");
+
+            //Console.WriteLine(await ReplaceRenderer.ParseAsync("hi @UserName,年龄： @Age，小名：@XiaoName 。", new { UserName = "测试", Age = 19, XiaoName = "小明" }));
+        }
+    }
+}
---
+++ Utils/Utils/ReplaceRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    /// <summary>
+    /// 替换渲染器
+    /// 使用方法：await ReplaceRenderer.ParseAsync("hi @UserName,年龄： @Age，小名：@XiaoName 。", new { UserName = "测试", Age = 19, XiaoName = "小明" })
+    /// </summary>
+    public class ReplaceRenderer
+    {
+        public static string Parse<T>(string template, T model)
+        {
+            foreach (PropertyInfo pi in model.GetType().GetRuntimeProperties())
+            {
+                template = template.Replace($"@{pi.Name}", pi.GetValue(model)?.ToString());
+            }
+
+            return template;
+        }
+
+        public static Task<string> ParseAsync<T>(string template, T model)
+        {
+            return Task.FromResult(Parse(template, model));
+        }
+    }
+}
