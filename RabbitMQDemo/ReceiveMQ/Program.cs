9d42ab667ca94480488971beae650e812a1a7c08
华营杰 <jasonhua12@163.com>
2019-06-24 18:27:10 +0800

    增加引用EasyNetQ库实现RabbitMQ

--- RabbitMQDemo/ReceiveMQ/Program.cs
+++ RabbitMQDemo/ReceiveMQ/Program.cs
@@ -1,4 +1,5 @@
-﻿using RabbitMQ.Client;
+﻿using EasyNetQ;
+using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
 using System.Text;
@@ -14,7 +15,15 @@ namespace ReceiveMQ
         static void Main(string[] args)
         {
             Console.WriteLine("销售者开始运行");
+            //OriginalMethod();
+            EasyNetQMethod();
+        }
 
+        /// <summary>
+        /// 原生方法
+        /// </summary>
+        static void OriginalMethod()
+        {
             var factory = new ConnectionFactory() { HostName = "localhost", Port = 32850, UserName = "test", Password = "test123" };
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
@@ -34,7 +43,8 @@ namespace ReceiveMQ
                     Console.WriteLine("接收信息：{0}", message);
                 };
 
-                while (true) {
+                while (true)
+                {
 
                     channel.BasicConsume(queue: "hello",
                                          autoAck: true,
@@ -44,7 +54,24 @@ namespace ReceiveMQ
                     Console.WriteLine("每隔10秒接收一次");
                 }
             }
-
         }
+
+        /// <summary>
+        /// 使用EasyNetQ库之后的方法
+        /// </summary>
+        static void EasyNetQMethod()
+        {
+            using (var bus = RabbitHutch.CreateBus("host=localhost;port=32850;username=test;password=test123"))
+            {
+                while (true)
+                {
+                    bus.Subscribe<string>("hello", (message) =>
+                    {
+                        Console.WriteLine("接收信息：{0}", message);
+                    });
+                }
+            }
+        }
+
     }
 }
--- RabbitMQDemo/SendMQ/Program.cs
+++ RabbitMQDemo/SendMQ/Program.cs
@@ -1,4 +1,5 @@
-﻿using RabbitMQ.Client;
+﻿using EasyNetQ;
+using RabbitMQ.Client;
 using System;
 using System.Text;
 using System.Threading;
@@ -13,7 +14,14 @@ namespace SendMQ
         static void Main(string[] args)
         {
             Console.WriteLine("生产者开始运行");
+            EasyNetQMethod();
 
+        }
+
+        /// <summary>
+        /// 原生方法
+        /// </summary>
+        static void OriginalMethod() {
             var factory = new ConnectionFactory() { HostName = "localhost", Port = 32850, UserName = "test", Password = "test123" };
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
@@ -42,7 +50,28 @@ namespace SendMQ
                     Thread.Sleep(TimeSpan.FromSeconds(rd.Next(5)));
                 }
             }
-
         }
+
+        /// <summary>
+        /// 使用EasyNetQ库之后的方法
+        /// </summary>
+        static void EasyNetQMethod()
+        {
+            using (var bus = RabbitHutch.CreateBus("host=localhost;port=32850;username=test;password=test123"))
+            {
+                //模拟生成者
+                Random rd = new Random();
+                while (true)
+                {
+                    string message = $"你好，{rd.Next(10)}。";
+                    bus.Publish(message, "hello");
+                    Console.WriteLine("发送信息：{0}", message);
+
+                    //随机休眠
+                    Thread.Sleep(TimeSpan.FromSeconds(rd.Next(5)));
+                }
+            }
+        }
+
     }
 }
